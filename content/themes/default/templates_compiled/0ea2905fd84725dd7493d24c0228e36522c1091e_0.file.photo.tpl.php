fe5a933308293bbd456f3d4e9ac2fc487b720cb0
ferry-acx <ferry.acxhub@gmail.com>
2023-11-06 21:55:19 +0800

    init

---
+++ content/themes/default/templates_compiled/0ea2905fd84725dd7493d24c0228e36522c1091e_0.file.photo.tpl.php
@@ -0,0 +1,85 @@
+<?php
+/* Smarty version 4.3.1, created on 2023-07-26 07:35:36
+  from '/home/vfcacxhub/public_html/content/themes/default/templates/photo.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.3.1',
+  'unifunc' => 'content_64c0ccc8da4320_29411968',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '0ea2905fd84725dd7493d24c0228e36522c1091e' => 
+    array (
+      0 => '/home/vfcacxhub/public_html/content/themes/default/templates/photo.tpl',
+      1 => 1689358234,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+    'file:_head.tpl' => 1,
+    'file:_header.tpl' => 1,
+    'file:_sidebar.tpl' => 1,
+    'file:__feeds_post_photo.tpl' => 1,
+    'file:_ads_campaigns.tpl' => 1,
+    'file:_ads.tpl' => 1,
+    'file:_widget.tpl' => 1,
+    'file:_footer.tpl' => 1,
+  ),
+),false)) {
+function content_64c0ccc8da4320_29411968 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_subTemplateRender('file:_head.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+$_smarty_tpl->_subTemplateRender('file:_header.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
+
+<?php $_smarty_tpl->_assignInScope('post', $_smarty_tpl->tpl_vars['photo']->value['post']);?>
+
+<!-- page content -->
+<div class="<?php if ($_smarty_tpl->tpl_vars['system']->value['fluid_design']) {?>container-fluid<?php } else { ?>container<?php }?> mt20 sg-offcanvas">
+  <div class="row">
+
+    <!-- side panel -->
+    <div class="col-12 d-block d-md-none sg-offcanvas-sidebar mt20">
+      <?php $_smarty_tpl->_subTemplateRender('file:_sidebar.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
+    </div>
+    <!-- side panel -->
+
+    <!-- content panel -->
+    <div class="col-12 sg-offcanvas-mainbar">
+      <div class="row">
+        <!-- left panel -->
+        <div class="col-md-8 col-lg-6 offset-lg-3">
+          <div class="post" data-id="<?php if ($_smarty_tpl->tpl_vars['photo']->value['is_single']) {
+echo $_smarty_tpl->tpl_vars['post']->value['post_id'];
+} else {
+echo $_smarty_tpl->tpl_vars['photo']->value['photo_id'];
+}?>">
+            <?php $_smarty_tpl->_subTemplateRender('file:__feeds_post_photo.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array('standalone'=>true), 0, false);
+?>
+          </div>
+        </div>
+        <!-- left panel -->
+
+        <!-- right panel -->
+        <div class="col-md-4 col-lg-3">
+          <?php $_smarty_tpl->_subTemplateRender('file:_ads_campaigns.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
+          <?php $_smarty_tpl->_subTemplateRender('file:_ads.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
+          <?php $_smarty_tpl->_subTemplateRender('file:_widget.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
+        </div>
+        <!-- right panel -->
+      </div>
+    </div>
+    <!-- content panel -->
+
+  </div>
+</div>
+<!-- page content -->
+
+<?php $_smarty_tpl->_subTemplateRender('file:_footer.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+}
+}
---
+++ content/themes/default/templates_compiled/ce5fed55a6d5bffade1d6366512c8840331067c1_0.file.live.svg.php
@@ -0,0 +1,25 @@
+<?php
+/* Smarty version 4.3.1, created on 2023-07-26 01:46:49
+  from '/home/vfcacxhub/public_html/content/themes/default/images/svg/live.svg' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.3.1',
+  'unifunc' => 'content_64c07b093fa933_01543003',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'ce5fed55a6d5bffade1d6366512c8840331067c1' => 
+    array (
+      0 => '/home/vfcacxhub/public_html/content/themes/default/images/svg/live.svg',
+      1 => 1689358230,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_64c07b093fa933_01543003 (Smarty_Internal_Template $_smarty_tpl) {
+?><svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" width="512" height="512" x="0" y="0" viewBox="0 0 32 32" style="enable-background:new 0 0 512 512" xml:space="preserve" class=""><g><path fill="#3cacb6" d="M16 18.4c-2.978 0-5.4-2.423-5.4-5.4s2.423-5.4 5.4-5.4 5.4 2.423 5.4 5.4-2.423 5.4-5.4 5.4zm0-8.801c-1.875 0-3.4 1.525-3.4 3.4s1.525 3.4 3.4 3.4 3.4-1.525 3.4-3.4-1.525-3.4-3.4-3.4z" data-original="#3cacb6"></path><path fill="#5e72e4" d="M26.9 13c0-6.011-4.89-10.9-10.9-10.9S5.1 6.989 5.1 13c0 4.89 3.236 9.037 7.68 10.414L11.276 28h-1.275a1 1 0 1 0 0 2h12a1 1 0 1 0 0-2h-1.276l-1.504-4.586c4.444-1.377 7.68-5.524 7.68-10.414zM13.379 28l1.37-4.177c.411.047.827.077 1.25.077s.839-.03 1.249-.077L18.618 28h-5.24zM7.1 13c0-4.907 3.993-8.9 8.9-8.9s8.9 3.993 8.9 8.9-3.993 8.9-8.9 8.9-8.9-3.993-8.9-8.9z" data-original="#06474d" class=""></path></g></svg><?php }
+}
